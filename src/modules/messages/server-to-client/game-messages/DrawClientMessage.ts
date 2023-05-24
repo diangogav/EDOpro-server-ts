2c2d202aa5b2d0807ef413f02005f066f735c898
DiangoGav <diangogavidia@gmail.com>
2023-05-24 18:34:26 -0400

    Fix DrawClientMessage Header

--- src/modules/messages/server-to-client/game-messages/DrawClientMessage.ts
+++ src/modules/messages/server-to-client/game-messages/DrawClientMessage.ts
@@ -1,6 +1,6 @@
 export class DrawClientMessage {
 	static create({ buffer }: { buffer: Buffer }): Buffer {
-		const header = Buffer.from([0x08, 0x00, 0x01]);
+		const header = Buffer.from([0x2f, 0x00, 0x01]);
 
 		return Buffer.concat([header, buffer]);
 	}
