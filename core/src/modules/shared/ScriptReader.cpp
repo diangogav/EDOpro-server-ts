bcc09adefdd984dd09bb2d50a39477a603c22c85
Diango Gavidia <diangogavidia@gmail.com>
2023-06-14 23:38:42 -0400

    Remove Script Reader global paths

--- core/src/modules/shared/ScriptReader.cpp
+++ core/src/modules/shared/ScriptReader.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "ScriptReader.h"
 #include "FileReader.h"
+#include <filesystem>
+namespace fs = std::filesystem;
 
 ScriptReader::ScriptReader(OCGRepository repository) : repository(repository) {}
 
@@ -12,15 +14,9 @@ int ScriptReader::handle(void *payload, void *duel, const char *name)
 
 int ScriptReader::read(void *duel, const char *name)
 {
-  printf("script name\n");
-  printf(name);
-  printf("\n");
   FileReader reader;
-  std::string filePath = "/home/diango/code/edo-pro-server-ts/core/scripts/";
-  filePath += name;
-  std::vector<char> buffer = reader.read(filePath.c_str());
+  fs::path path = fs::current_path().string() + "/core/scripts/";
+  std::vector<char> buffer = reader.read((path.string() += name).c_str());
   int loaded = repository.loadScript(duel, buffer.data(), buffer.size(), name);
-  printf("%d", loaded);
-  printf("\n");
   return loaded;
 }
