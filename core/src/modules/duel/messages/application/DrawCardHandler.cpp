408416e75dadc50c9dc92a7a2be8b029c0723b24
Diango Gavidia <diangogavidia@gmail.com>
2023-06-20 20:19:09 -0400

    Adding MSG_SELECT_CARD, MSG_SELECT_UNSELECT_CARD, MSG_SHUFFLE_HAND and MSG_SHUFFLE_EXTRA to DrawCardHandler

--- core/src/modules/duel/messages/application/DrawCardHandler.cpp
+++ core/src/modules/duel/messages/application/DrawCardHandler.cpp
@@ -3,18 +3,15 @@
 #include "../../../shared/DuelLocations.h"
 #include "../../../shared/QueryRequest.h"
 
-template<>
-constexpr LocInfo Read(const uint8_t*& ptr) noexcept
+template <>
+constexpr LocInfo Read(const uint8_t *&ptr) noexcept
 {
-	return LocInfo
-	{
-		Read<uint8_t>(ptr),
-		Read<uint8_t>(ptr),
-		Read<uint32_t>(ptr),
-		Read<uint32_t>(ptr)
-	};
+  return LocInfo{
+      Read<uint8_t>(ptr),
+      Read<uint8_t>(ptr),
+      Read<uint32_t>(ptr),
+      Read<uint32_t>(ptr)};
 }
-
 
 std::vector<uint8_t> DrawCardHandler::handle(uint8_t team, std::vector<uint8_t> message)
 {
@@ -28,12 +25,37 @@ std::vector<uint8_t> DrawCardHandler::handle(uint8_t team, std::vector<uint8_t> 
       return true;
     return false;
   };
+  auto ClearLocInfoArray = [](uint32_t count, uint8_t team, uint8_t *&ptr)
+  {
+    for (uint32_t i = 0U; i < count; i++)
+    {
+      ptr += 4U; // Card code
+      const auto info = Read<LocInfo>(ptr);
+      if (team != info.con)
+      {
+        ptr -= LocInfo::SIZE + 4U;
+        Write<uint32_t>(ptr, 0U);
+        ptr += LocInfo::SIZE;
+      }
+    }
+  };
 
   auto *ptr = message.data();
   ptr++; // type ignored
 
-  if(message[0U] == MSG_SET){
+  if (message[0U] == MSG_SET)
+  {
     Write<uint32_t>(ptr, 0U);
+  }
+
+  if (message[0U] == MSG_SHUFFLE_HAND || message[0U] == MSG_SHUFFLE_EXTRA)
+  {
+    if (Read<uint8_t>(ptr) != team)
+    {
+      auto count = Read<uint32_t>(ptr);
+      for (uint32_t i = 0U; i < count; i++)
+        Write<uint32_t>(ptr, 0U);
+    }
   }
 
   if (message[0U] == MSG_DRAW)
@@ -59,6 +81,40 @@ std::vector<uint8_t> DrawCardHandler::handle(uint8_t team, std::vector<uint8_t> 
       ptr -= 4U + (LocInfo::SIZE * 2U);
       Write<uint32_t>(ptr, 0U);
     }
+  }
+
+  if (message[0U] == MSG_SELECT_CARD)
+  {
+    ptr += 1U + 1U + 4U + 4U;
+    auto count = Read<uint32_t>(ptr);
+    ClearLocInfoArray(count, team, ptr);
+  }
+
+  if (message[0U] == MSG_SELECT_TRIBUTE)
+  {
+    ptr += 1U + 1U + 4U + 4U;
+    auto count = Read<uint32_t>(ptr);
+    for (uint32_t i = 0; i < count; i++)
+    {
+      ptr += 4U; // Card code
+      const auto con = Read<uint8_t>(ptr);
+      if (team != con)
+      {
+        ptr -= 4U + 1U;
+        Write<uint32_t>(ptr, 0U);
+        ptr += 1U;
+      }
+      ptr += 1U + 4U + 1U; // loc, seq, release_param
+    }
+  }
+
+  if (message[0U] == MSG_SELECT_UNSELECT_CARD)
+  {
+    ptr += 1U + 1U + 1U + 4U + 4U;
+    auto count1 = Read<uint32_t>(ptr);
+    ClearLocInfoArray(count1, team, ptr);
+    auto count2 = Read<uint32_t>(ptr);
+    ClearLocInfoArray(count2, team, ptr);
   }
   return message;
 }
