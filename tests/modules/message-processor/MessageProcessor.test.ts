a22e0051ba2322d33f11f51640b3c9e2d5493d30
Diango Gavidia <diangogavidia@gmail.com>
2023-08-08 23:12:45 -0400

    Linter

--- tests/modules/message-processor/MessageProcessor.test.ts
+++ tests/modules/message-processor/MessageProcessor.test.ts
@@ -1,5 +1,5 @@
-import { MessageProcessor } from "../../../src/modules/messages/MessageProcessor";
 import { Commands } from "../../../src/modules/messages/domain/Commands";
+import { MessageProcessor } from "../../../src/modules/messages/MessageProcessor";
 
 describe("Message Processor", () => {
 	let data: Buffer;
