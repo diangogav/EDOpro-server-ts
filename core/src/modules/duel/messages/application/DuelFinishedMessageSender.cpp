2516033170631b85aa9b1846cc3a110d5057abeb
Diango Gavidia <diangogavidia@gmail.com>
2023-08-24 23:55:39 -0400

    Remove innecesary c++ code

--- core/src/modules/duel/messages/application/DuelFinishedMessageSender.cpp
+++
@@ -1,9 +0,0 @@
-#include "DuelFinishedMessageSender.h"
-
-void DuelFinishedMessageSender::send(uint8_t reason, uint8_t winner)
-{
-  std::string payload = "CMD:FINISH|";
-  payload += std::to_string(static_cast<int>(reason)) + "|";
-  payload += std::to_string(static_cast<int>(winner)) + "|";
-  std::cout << payload << std::endl;
-}
--- core/src/modules/duel/messages/application/TimeLimitMessageSender.cpp
+++
@@ -1,9 +0,0 @@
-#include "TimeLimitMessageSender.h"
-
-void TimeLimitMessageSender::send(uint8_t team, uint16_t timeLeft)
-{
-  std::string payload = "CMD:TIME|";
-  payload += std::to_string(team) + "|";
-  payload += std::to_string(timeLeft) + "|";
-  std::cout << payload << std::endl;
-}
