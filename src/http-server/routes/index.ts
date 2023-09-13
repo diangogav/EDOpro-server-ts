a939242ba0fc1e6406e5eb59045d15df05599e9d
Diango Gavidia <diangogavidia@gmail.com>
2023-09-13 18:46:05 -0400

    endpoint for analize memory leaks

--- src/http-server/routes/index.ts
+++ src/http-server/routes/index.ts
@@ -1,7 +1,21 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { Express } from "express";
+import heapdump from "heapdump";
 
 import { GetRoomListController } from "../controllers/GetRoomListController";
 
 export function loadRoutes(app: Express): void {
 	app.get("/api/getrooms", (req, res) => new GetRoomListController().run(req, res));
+	app.get("/api/snapshot", (req, res) => {
+		heapdump.writeSnapshot((error, filename) => {
+			if (error) {
+				return res.status(500).json(error);
+			}
+
+			console.log(`Heap snapshot written to ${filename}`);
+
+			return res.status(200).json({});
+		});
+	});
 }
