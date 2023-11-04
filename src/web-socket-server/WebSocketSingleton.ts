a6a3280d3989fd454e0730a8339960786710095d
Diango Gavidia <diangogavidia@gmail.com>
2023-11-04 12:28:33 -0400

    !!

--- src/web-socket-server/WebSocketSingleton.ts
+++ src/web-socket-server/WebSocketSingleton.ts
@@ -1,17 +1,24 @@
+/* eslint-disable no-use-before-define */
 import { readFileSync } from "fs";
-import https from "https";
+import { createServer } from "https";
+import path from "path";
 import WebSocket, { WebSocketServer } from "ws";
 
 import RoomList from "../modules/room/infrastructure/RoomList";
 import { WebSocketMessage } from "./WebSocketMessage";
 
 class WebSocketSingleton {
-	// eslint-disable-next-line no-use-before-define
 	private static instance: WebSocketSingleton | null = null;
 	private readonly wss: WebSocketServer | null = null;
 
-	private constructor(httpsServer: https.Server) {
-		this.wss = new WebSocketServer({ server: httpsServer });
+	private constructor(port: number) {
+		const root = path.resolve(__dirname, "../../");
+		const server = createServer({
+			cert: readFileSync(`${root}/letsencrypt/live/server.evolutionygo.com/cert.pem`),
+			key: readFileSync(`${root}/letsencrypt/live/server.evolutionygo.com/privkey.pem`),
+		});
+
+		this.wss = new WebSocketServer({ server });
 		this.wss.on("connection", (ws: WebSocket) => {
 			ws.send(
 				JSON.stringify({
@@ -20,12 +27,12 @@ class WebSocketSingleton {
 				})
 			);
 		});
+		server.listen(port);
 	}
 
-	public static getInstance(httpsServer: https.Server): WebSocketSingleton {
+	public static getInstance(): WebSocketSingleton {
 		if (!WebSocketSingleton.instance) {
-			console.log("WebSocket Server Up!");
-			WebSocketSingleton.instance = new WebSocketSingleton(httpsServer);
+			WebSocketSingleton.instance = new WebSocketSingleton(4000);
 		}
 
 		return WebSocketSingleton.instance;
@@ -43,19 +50,3 @@ class WebSocketSingleton {
 }
 
 export default WebSocketSingleton;
-
-// Crear el servidor HTTPS
-const options = {
-	cert: readFileSync(`./letsencrypt/live/server.evolutionygo.com/cert.pem`),
-	key: readFileSync(`./letsencrypt/live/server.evolutionygo.com/privkey.pem`),
-};
-
-const server = https.createServer(options, (_req, _res) => {
-	// Manejar solicitudes HTTP si es necesario
-});
-
-// Iniciar el servidor Express en el servidor HTTPS
-server.listen(4000, () => {
-	console.log("Server is running on port 4000");
-	WebSocketSingleton.getInstance(server);
-});
