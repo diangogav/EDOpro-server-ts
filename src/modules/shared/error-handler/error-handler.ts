16a1d9688d24aab7178a5b56e9359dccd6ca8076
Diango Gavidia <diangogavidia@gmail.com>
2023-11-21 13:22:44 -0400

    Remove Pino instance at global error handler

--- src/modules/shared/error-handler/error-handler.ts
+++ src/modules/shared/error-handler/error-handler.ts
@@ -1,9 +1,3 @@
-import { Pino } from "../logger/infrastructure/Pino";
-
-const logger = new Pino();
-
 process.on("uncaughtException", (error: Error) => {
 	console.error("Excepción no capturada:", error);
-	logger.error(error);
-	// Aquí puedes realizar acciones adicionales, como guardar registros de errores, limpiar recursos, etc.
 });
