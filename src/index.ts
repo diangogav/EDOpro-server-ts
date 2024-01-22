93f346d0915ffd336a95098d197d9e41d0afdebb
Diango Gavidia <diangogavidia@gmail.com>
2024-01-22 17:30:34 -0400

    Comments banlist loader

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import "./modules/shared/error-handler/error-handler";
 
 import { Server } from "./http-server/Server";
-import BanListMemoryRepository from "./modules/ban-list/infrastructure/BanListMemoryRepository";
 import { SQLiteTypeORM } from "./modules/shared/db/postgres/infrastructure/SQLiteTypeORM";
 import { Pino } from "./modules/shared/logger/infrastructure/Pino";
 import { HostServer } from "./socket-server/HostServer";
@@ -19,7 +18,7 @@ async function start(): Promise<void> {
 	const database = new SQLiteTypeORM();
 	// const banListLoader = new BanListLoader();
 	// await banListLoader.loadDirectory("./banlists");
-	await BanListMemoryRepository.backup();
+	// await BanListMemoryRepository.backup();
 	await database.connect();
 	await database.initialize();
 	await server.initialize();
