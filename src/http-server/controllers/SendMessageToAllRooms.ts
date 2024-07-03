587bc8e9891bd06eb6f3250d938957df7f334459
Diego Cornejo <diegof.cornejo@gmail.com>
2024-07-03 09:36:38 -0600

    feat: send message to all rooms and clients

--- src/http-server/controllers/SendMessageToAllRooms.ts
+++ src/http-server/controllers/SendMessageToAllRooms.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 import MercuryRoomList from "../../mercury/room/infrastructure/MercuryRoomList";
+import { ServerMessageClientMessage } from "../../modules/messages/server-to-client/ServerMessageClientMessage";
 import RoomList from "../../modules/room/infrastructure/RoomList";
 import { Logger } from "../../modules/shared/logger/domain/Logger";
 
@@ -15,11 +16,14 @@ export class SendMessageToAllRooms {
 	// eslint-disable-next-line @typescript-eslint/require-await
 	async run(req: Request, response: Response): Promise<void> {
 		const payload = req.body as CreateMessageRequest;
-		const rooms = RoomList.getRooms().map((room) => room.toPresentation());
-		const mercuryRooms = MercuryRoomList.getRooms().map((room) => room.toPresentation());
-		const allRooms = [...mercuryRooms, ...rooms];
-		// TODO: Implement the logic to send a message to all rooms
-		// Find the socket by room id, this can be done by a method that receives the room id and returns the socket or a methos that retunrs a room list with the socket
-		response.status(200).json({ rooms: allRooms, ...payload });
+		const rooms = [...RoomList.getRooms(), ...MercuryRoomList.getRooms()];
+		for (const room of rooms) {
+			const allClients = [...room.clients, ...room.spectators];
+			for (const client of allClients) {
+				const socket = client.socket;
+				socket.send(ServerMessageClientMessage.create(payload.message));
+			}
+		}
+		response.status(200).json({ ...payload });
 	}
 }
