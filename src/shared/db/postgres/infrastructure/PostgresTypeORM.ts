f834dccc0470e91514c83a855a5eb4847f91b409
Diango Gavidia <diangogavidia@gmail.com>
2024-09-02 18:57:32 -0400

    feat: :beers: Add close method to postgres database

--- src/shared/db/postgres/infrastructure/PostgresTypeORM.ts
+++ src/shared/db/postgres/infrastructure/PostgresTypeORM.ts
@@ -13,4 +13,8 @@ export class PostgresTypeORM implements Database {
 	async connect(): Promise<void> {
 		await this.dataSource.initialize();
 	}
+
+	async close(): Promise<void> {
+		await this.dataSource.destroy();
+	}
 }
