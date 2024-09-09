ce5033ed4e2c5f14b0ce8d9af92e7d584f9fd78a
Diango Gavidia <diangogavidia@gmail.com>
2024-09-09 06:49:40 -0400

    refactor: :fire: Remove unused room code

--- src/shared/stats/by-rank-rule/rank-rules/domain/RankRuleRepository.ts
+++
@@ -1,7 +0,0 @@
-import { RankRule } from "./RankRule";
-
-export interface RankRuleRepository {
-	get(): Promise<RankRule[]>;
-	findByRankPosition(rankPosition: number): Promise<RankRule>;
-	findByName(name: string): Promise<RankRule>;
-}
