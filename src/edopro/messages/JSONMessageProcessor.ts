bae67f1207e6df2e4fc7251f3dd52f98f12c5cbe
Diango Gavidia <diango.code@gmail.com>
2026-01-12 21:35:52 -0400

    fix: handle large messages in JSONMessageProcessor by using 4-byte size header

--- src/edopro/messages/JSONMessageProcessor.ts
+++ src/edopro/messages/JSONMessageProcessor.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-control-regex */
- 
+
 
 export interface JSONClientMessage {
 	data: string;
@@ -30,20 +30,20 @@ export class JSONMessageProcessor {
 		if (this._data.length) {
 			// this._previousMessage = this._data;
 		}
-		this._size = this.buffer.readUint16LE(0);
+		this._size = this.buffer.readUInt32LE(0);
 		this._data = this.buffer
-			.subarray(2, this._size + 4)
+			.subarray(4, this._size + 4)
 			.toString("utf-8")
 			.replace(/^\u0000+/, "");
 		this.buffer = this.buffer.subarray(this._size + 4);
 	}
 
 	isMessageReady(): boolean {
-		if (this.buffer.length === 0) {
+		if (this.buffer.length < 4) {
 			return false;
 		}
-		const messageSize = this.buffer.readInt16LE(0);
-		const length = this.buffer.length - 2;
+		const messageSize = this.buffer.readUInt32LE(0);
+		const length = this.buffer.length - 4;
 
 		return length >= messageSize;
 	}
