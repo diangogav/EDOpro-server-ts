46e1ff4c59c01c6f0703d2265c70b52d8f7eb0eb
Diango Gavidia <diango.code@gmail.com>
2026-01-21 21:57:14 -0400

    refactor: standardize formatting in TextVO class and remove obsolete CreateGameMessage test

--- src/shared/value-objects/TextVO.ts
+++ src/shared/value-objects/TextVO.ts
@@ -1,21 +1,21 @@
 export class TextVO {
-  readonly value: string;
+	readonly value: string;
 
-  constructor(text: Buffer) {
-    this.value = this.readUntilNullTerminator(text);
-  }
+	constructor(text: Buffer) {
+		this.value = this.readUntilNullTerminator(text);
+	}
 
-  private readUntilNullTerminator(buffer: Buffer): string {
-    let index = 0;
-    while (index < buffer.length) {
-      const charCode = buffer.readUInt16LE(index);
-      if (charCode === 0) {
-        break;
-      }
-      index += 2;
-    }
-    const utf16Buffer = buffer.slice(0, index);
+	private readUntilNullTerminator(buffer: Buffer): string {
+		let index = 0;
+		while (index < buffer.length) {
+			const charCode = buffer.readUInt16LE(index);
+			if (charCode === 0) {
+				break;
+			}
+			index += 2;
+		}
+		const utf16Buffer = buffer.slice(0, index);
 
-    return utf16Buffer.toString("utf16le");
-  }
+		return utf16Buffer.toString();
+	}
 }
