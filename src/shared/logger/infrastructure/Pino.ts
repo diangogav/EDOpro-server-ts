a7b9e6fe44e968918cd73e2c87bc15d01eecb74f
DiangoGav <diango.code@gmail.com>
2026-03-28 11:01:36 -0400

    feat(logger): add warn method to Logger interface and implementations

--- src/shared/logger/infrastructure/Pino.ts
+++ src/shared/logger/infrastructure/Pino.ts
@@ -5,89 +5,108 @@ import type { Transform } from "stream";
 import { Logger } from "../domain/Logger";
 
 const color =
-	(number: number) =>
-	(value: unknown): string =>
-		`\x1b[${number}m${String(value)}\x1b[0m`;
+  (number: number) =>
+  (value: unknown): string =>
+    `\x1b[${number}m${String(value)}\x1b[0m`;
 
 function createPrettyStream(opts?: PrettyOptions): Transform {
-	return pretty({
-		...(opts ?? {}),
-		customPrettifiers: {
-			...(opts?.customPrettifiers as Record<string, (v: unknown) => string>),
-			roomId: color(33), // yellow
-			clientId: color(36), // cyan
-			clientName: color(35), // magenta
-			socketId: color(34), // blue
-			remoteAddress: color(90), // gray
-			file: color(34), // blue
-		},
-	});
+  return pretty({
+    ...(opts ?? {}),
+    customPrettifiers: {
+      ...(opts?.customPrettifiers as Record<string, (v: unknown) => string>),
+      roomId: color(33), // yellow
+      clientId: color(36), // cyan
+      clientName: color(35), // magenta
+      socketId: color(34), // blue
+      remoteAddress: color(90), // gray
+      file: color(34), // blue
+    },
+  });
 }
 
 export class Pino implements Logger {
-	private readonly logger: PinoLogger;
-	private readonly fileLogger: PinoLogger;
+  private readonly logger: PinoLogger;
+  private readonly fileLogger: PinoLogger;
 
-	constructor(logger?: PinoLogger, fileLogger?: PinoLogger) {
-		if (logger && fileLogger) {
-			this.logger = logger;
-			this.fileLogger = fileLogger;
-		} else {
-			this.logger = pino({ level: "debug" }, createPrettyStream({ colorize: true }));
+  constructor(logger?: PinoLogger, fileLogger?: PinoLogger) {
+    if (logger && fileLogger) {
+      this.logger = logger;
+      this.fileLogger = fileLogger;
+    } else {
+      this.logger = pino(
+        { level: "debug" },
+        createPrettyStream({ colorize: true }),
+      );
 
-			this.fileLogger = pino({
-				level: "debug",
-				transport: {
-					target: "pino/file",
-					options: { destination: "app.log" },
-				},
-			});
-		}
-	}
+      this.fileLogger = pino({
+        level: "debug",
+        transport: {
+          target: "pino/file",
+          options: { destination: "app.log" },
+        },
+      });
+    }
+  }
 
-	debug(message: unknown, context?: Record<string, unknown>): void {
-		const msg = typeof message === "string" ? message : JSON.stringify(message);
+  debug(message: unknown, context?: Record<string, unknown>): void {
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
 
-		if (context && typeof context === "object") {
-			this.logger.debug({ ...context }, msg);
+    if (context && typeof context === "object") {
+      this.logger.debug({ ...context }, msg);
 
-			// this.fileLogger.debug({ ...context }, msg);
-			return;
-		}
+      // this.fileLogger.debug({ ...context }, msg);
+      return;
+    }
 
-		this.logger.debug(msg);
-	}
+    this.logger.debug(msg);
+  }
 
-	error(error: string | Error, context?: Record<string, unknown>): void {
-		const errObj = error instanceof Error ? error : new Error(String(error));
+  error(error: string | Error, context?: Record<string, unknown>): void {
+    const errObj = error instanceof Error ? error : new Error(String(error));
 
-		if (context && typeof context === "object") {
-			this.logger.error({ ...context, err: errObj.stack ?? errObj.message }, errObj.message);
+    if (context && typeof context === "object") {
+      this.logger.error(
+        { ...context, err: errObj.stack ?? errObj.message },
+        errObj.message,
+      );
 
-			// this.fileLogger.error({ ...context, err: errObj.stack ?? errObj.message }, errObj.message);
-			return;
-		}
+      // this.fileLogger.error({ ...context, err: errObj.stack ?? errObj.message }, errObj.message);
+      return;
+    }
 
-		this.logger.error(errObj.message);
-	}
+    this.logger.error(errObj.message);
+  }
 
-	info(message: unknown, context?: Record<string, unknown>): void {
-		const msg = typeof message === "string" ? message : JSON.stringify(message);
+  info(message: unknown, context?: Record<string, unknown>): void {
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
 
-		if (context && typeof context === "object") {
-			this.logger.info({ ...context }, msg);
+    if (context && typeof context === "object") {
+      this.logger.info({ ...context }, msg);
 
-			// this.fileLogger.info({ ...context }, msg);
-			return;
-		}
+      // this.fileLogger.info({ ...context }, msg);
+      return;
+    }
 
-		this.logger.info(msg);
-	}
+    this.logger.info(msg);
+  }
 
-	child(bindings: Record<string, unknown>): Logger {
-		const childLogger = this.logger.child(bindings);
-		const childFileLogger = this.fileLogger.child(bindings);
+  warn(message: unknown, context?: Record<string, unknown>): void {
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
 
-		return new Pino(childLogger, childFileLogger);
-	}
+    if (context && typeof context === "object") {
+      this.logger.warn({ ...context }, msg);
+
+      // this.fileLogger.warn({ ...context }, msg);
+      return;
+    }
+
+    this.logger.warn(msg);
+  }
+
+  child(bindings: Record<string, unknown>): Logger {
+    const childLogger = this.logger.child(bindings);
+    const childFileLogger = this.fileLogger.child(bindings);
+
+    return new Pino(childLogger, childFileLogger);
+  }
 }
--- src/shared/logger/infrastructure/Winston.ts
+++ src/shared/logger/infrastructure/Winston.ts
@@ -3,46 +3,52 @@ import winston, { Logger as WinstonLogger } from "winston";
 import { Logger } from "../domain/Logger";
 
 export class Winston implements Logger {
-	private readonly logger: WinstonLogger;
-	private readonly debugLogger: WinstonLogger;
+  private readonly logger: WinstonLogger;
+  private readonly debugLogger: WinstonLogger;
 
-	constructor() {
-		this.logger = winston.createLogger({
-			format: winston.format.combine(
-				winston.format.prettyPrint(),
-				winston.format.errors({ stack: true }),
-				winston.format.splat(),
-				winston.format.colorize(),
-				winston.format.simple()
-			),
-			transports: [new winston.transports.Console()],
-		});
+  constructor() {
+    this.logger = winston.createLogger({
+      format: winston.format.combine(
+        winston.format.prettyPrint(),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+      transports: [new winston.transports.Console()],
+    });
 
-		this.debugLogger = winston.createLogger({
-			format: winston.format.combine(winston.format.json()),
-			transports: [new winston.transports.Console({ level: "debug" })],
-		});
-	}
+    this.debugLogger = winston.createLogger({
+      format: winston.format.combine(winston.format.json()),
+      transports: [new winston.transports.Console({ level: "debug" })],
+    });
+  }
 
-	child(bindings: Record<string, unknown>): Logger {
-		throw new Error("Method not implemented.");
-	}
+  child(bindings: Record<string, unknown>): Logger {
+    throw new Error("Method not implemented.");
+  }
 
-	debug(message: unknown, context?: Record<string, unknown>): void {
-		const meta = context ?? {};
-		const msg = typeof message === "string" ? message : JSON.stringify(message);
-		this.debugLogger.debug(msg, meta);
-	}
+  debug(message: unknown, context?: Record<string, unknown>): void {
+    const meta = context ?? {};
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
+    this.debugLogger.debug(msg, meta);
+  }
 
-	error(error: string | Error, context?: Record<string, unknown>): void {
-		const meta = context ?? {};
-		const errObj = error instanceof Error ? error : new Error(String(error));
-		this.logger.error(errObj.message, { ...meta, stack: errObj.stack });
-	}
+  error(error: string | Error, context?: Record<string, unknown>): void {
+    const meta = context ?? {};
+    const errObj = error instanceof Error ? error : new Error(String(error));
+    this.logger.error(errObj.message, { ...meta, stack: errObj.stack });
+  }
 
-	info(message: unknown, context?: Record<string, unknown>): void {
-		const meta = context ?? {};
-		const msg = typeof message === "string" ? message : JSON.stringify(message);
-		this.logger.info(msg, meta);
-	}
+  info(message: unknown, context?: Record<string, unknown>): void {
+    const meta = context ?? {};
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
+    this.logger.info(msg, meta);
+  }
+
+  warn(message: unknown, context?: Record<string, unknown>): void {
+    const meta = context ?? {};
+    const msg = typeof message === "string" ? message : JSON.stringify(message);
+    this.logger.warn(msg, meta);
+  }
 }
